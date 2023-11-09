b2fffde2a55f85cfdfad3ca7647712ece1b847f3
anvarturakulov <anvarsam@mail.ru>
2023-11-09 13:58:33 +0500

    works with file system

--- weather.js
+++ weather.js
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
 
 import { getArgs } from "./helpers/args.js"
-import { printHelp } from "./services/log.service.js"
+import { printHelp, printError, printSuccess } from "./services/log.service.js"
 import { saveKeyValue } from "./services/storage.service.js"
 
+const saveToken = async (token) => {
+    try {
+        await saveKeyValue('token', token)
+        printSuccess('Токен сохранен') 
+    } catch (e) {
+        printError(e.message)
+    }
+}
 
 const initCLI = () => {
     const args = getArgs(process.argv)
-    // console.log(args)
     if (args.h) {
         printHelp()
     }
@@ -17,10 +24,10 @@ const initCLI = () => {
     }
 
     if (args.t) {
-        saveKeyValue('token', args.t)
+        return saveToken(args.t)
     } 
 
-    // Выыести погоду
+    // Вывести погоду
 
 }
 
